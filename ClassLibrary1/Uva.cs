a698dabf5c3b7976f88594e04b07493c51f6dd1e
Luciano Gnocchi <lucianoGnocchi@gmail.com>
2018-10-26 15:08:36 -0300

    no message

---
+++ ClassLibrary1/Uva.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Uva
+    {
+        public int diasNecesariosCosecha { get; set; }
+        public string origen { get; set; }
+        public int porcentajeAzucar { get; set; }
+        public string epoca { get; set; }
+        public int diasSol { get; set; }
+        public virtual string calidad()
+        {
+            return "";
+        }
+    }
+    public class Tannat : Uva
+    {
+        public bool polilla_vid { get; set; }
+
+        override
+        public string calidad()
+        {
+            if (!polilla_vid && diasSol > 250)
+                return "Optima";
+            if ((polilla_vid && diasSol > 250) || !polilla_vid)
+                return "Media";
+            else
+                return "Mala calidad";
+        }
+
+
+    }
+    public class Moscatel : Uva
+    {
+        public bool saltaMontes { get; set; }
+        override
+public string calidad()
+        {
+            if (!saltaMontes && diasSol > 200)
+                return "Optima";
+            if ((saltaMontes && diasSol > 200) || !saltaMontes)
+                return "Media";
+            else
+                return "Mala calidad";
+        }
+
+    }
+    public class Chinche : Uva
+    {
+        public bool polilla_vid { get; set; }
+        public bool saltaMontes { get; set; }
+        override
+        public string calidad()
+        {
+            if (!saltaMontes && diasSol > 300 &&!polilla_vid)
+                return "Optima";
+            if ((saltaMontes && diasSol > 300 && !polilla_vid) || (!saltaMontes && diasSol > 300 && polilla_vid )|| (!saltaMontes && !polilla_vid))
+                return "Media";
+            else
+                return "Mala calidad";
+        }
+
+    }
+    public class NuevoTipo : Uva
+    {
+        override
+        public string calidad()
+        {
+            if (diasSol>100)
+                return "Optima";
+            else
+                return "Mala calidad";
+        }
+
+    }
+    public class Cosecha
+    {
+        public DateTime fechaInicio { get; set; }
+        public DateTime fechaFin { get; set; }
+        public int cantidad { get; set; }
+        public string Calidad { get; set; }
+    }
+
+
+}
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,39 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<Uva> miListaUva = new List<Uva>();
+            miListaUva.Add(new Chinche {origen="chileno", diasSol = 299, polilla_vid = false, saltaMontes = true });
+            miListaUva.Add(new Tannat { origen = "Uruguayo (iti)", diasSol = 251, polilla_vid = false });
+            miListaUva.Add(new NuevoTipo { origen = "laboratorio INAE", diasSol = 75 });
+            foreach (var item in miListaUva)
+            {
+                listBox1.Items.Add("Es de origen: " + item.origen + " y su calidad es: " + item.calidad());
+            }
+
+        }
+    }
+}
